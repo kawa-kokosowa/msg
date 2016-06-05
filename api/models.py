820637cb398fa053d82ef8fc3b85d854251bd408
Lily Lemmer <lily.lemmer@ave81.com>
2016-06-05 16:26:15 -0500

    add authentication

--- api/models.py
+++ api/models.py
@@ -2,10 +2,44 @@ import datetime
 import sqlalchemy
 
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.security import (generate_password_hash,
+                               check_password_hash)
 
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    created = sqlalchemy.Column(sqlalchemy.DateTime,
+                                default=datetime.datetime.utcnow)
+    username = sqlalchemy.Column(sqlalchemy.String(30), unique=True)
+    password_hash = sqlalchemy.Column(sqlalchemy.String())
+    bio = sqlalchemy.Column(sqlalchemy.String())
+
+    def __init__(self, username, password, bio=None):
+        self.username = username
+        self.password_hash = self.hash_password(password)
+        self.bio = bio
+
+    @staticmethod
+    def hash_password(password):
+        return generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def to_dict(self):
+        """Return a dictionary representation of this
+        user, which keys are in jsonStyle.
+
+        """
+
+        return {'username': self.username,
+                'bio': self.bio,
+                'id': self.id,
+                'created': self.created.isoformat("T") + 'Z'}
 
 class Post(Base):
     """A submission to the site, just some text,
@@ -26,6 +60,11 @@ class Post(Base):
         return '<Post #%s>' % self.id
 
     def to_dict(self):
+        """Return a dictionary representation of this
+        user, which keys are in jsonStyle.
+
+        """
+
         return {'id': self.id,
                 'text': self.text,
                 'created': self.created.isoformat("T") + 'Z'}
--- api/msgboard.py
+++ api/msgboard.py
@@ -21,9 +21,10 @@ import config
 import requests
 import flask_sqlalchemy
 
-from gevent.pywsgi import WSGIServer
 from gevent import monkey
+from gevent.pywsgi import WSGIServer
 from flask_limiter import Limiter
+from flask_httpauth import HTTPBasicAuth
 
 
 monkey.patch_all()  # NOTE: totally cargo culting this one
@@ -33,6 +34,36 @@ app.config.from_object("config")
 api = flask_restful.Api(app)
 limiter = Limiter(app)
 db = flask_sqlalchemy.SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(flask_restful.Resource):
+    """API tools for managing, creating, etc.,
+    users in the system.
+
+    """
+
+    def get(self, user_id):
+        """Get a specific user's info.
+
+        """
+
+        result = db.session.query(models.User).get(user_id)
+        return result.to_dict()
+
+    def post(self):
+        """Create a new user.
+
+        """
+
+        json_data = flask.request.get_json(force=True)
+        username = json_data['username']
+        password = json_data['password']
+        bio = json_data.get('bio')
+        new_user = models.User(username, password, bio=bio)
+        db.session.add(new_user)
+        db.session.commit()
+        return self.get(new_user.id)
 
 
 class Posts(flask_restful.Resource):
@@ -56,7 +87,12 @@ class Posts(flask_restful.Resource):
 
 class Post(flask_restful.Resource):
 
+    @auth.login_required
     def put(self, post_id):
+        """Edit an existing post.
+
+        """
+
         json_data = flask.request.get_json(force=True)
         text = json_data['text']
         result = db.session.query(models.Post).get(post_id)
@@ -64,7 +100,12 @@ class Post(flask_restful.Resource):
         db.session.commit()
         return self.get(post_id)
 
+    @auth.login_required
     def post(self):
+        """Create a new post.
+
+        """
+
         json_data = flask.request.get_json(force=True)
         text = json_data['text']
         new_post = models.Post(text)
@@ -72,12 +113,21 @@ class Post(flask_restful.Resource):
         db.session.commit()
         return self.get(new_post.id)
 
+    @auth.login_required
     def delete(self, post_id):
+        """Delete a specific post.
+
+        """
+
         result = db.session.query(models.Post).get(post_id)
         db.session.delete(result)
         db.session.commit()
 
     def get(self, post_id):
+        """Get a specific post.
+
+        """
+
         result = db.session.query(models.Post).get(post_id)
 
         if result:
@@ -85,11 +135,22 @@ class Post(flask_restful.Resource):
 
 
 class Stream(flask_restful.Resource):
+    """A live event stream (especiall for JavaScript
+    EventSource) for new posts.
+
+    """
 
     def get(self):
+        """Get events from the stream.
+
+        """
+
         return flask.Response(self.event(), mimetype="text/event-stream")
 
     def event(self):
+        """The actual event magic.
+
+        """
 
         with app.app_context():
 
@@ -118,6 +179,12 @@ class Stream(flask_restful.Resource):
                 gevent.sleep(app.config['SLEEP_RATE'])
 
 
+@auth.verify_password
+def get_password(username, password):
+    result = db.session.query(models.User).filter(models.User.username==username).first()
+    return result.check_password(password)
+
+
 def init_db():
     """For use on command line for setting up
     the database.
@@ -131,6 +198,7 @@ def init_db():
 api.add_resource(Post, '/post', '/post/<int:post_id>')
 api.add_resource(Posts, '/posts', '/posts/<int:page>')
 api.add_resource(Stream, '/stream')
+api.add_resource(User, '/user', '/user/<int:user_id>')
 
 
 if __name__ == '__main__':
@@ -140,5 +208,3 @@ if __name__ == '__main__':
         init_db()
 
     WSGIServer(('', 5000), app).serve_forever()
-
-    app.run(debug=True, threaded=True)
