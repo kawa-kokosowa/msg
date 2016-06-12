ae77ebc61aa69ecc814ea48e3eb03dabf8285c19
Lily Lemmer <lily.lemmer@ave81.com>
2016-06-11 21:26:43 -0500

    clean up

--- README.md
+++ README.md
@@ -9,16 +9,8 @@ The actual magic is done through the api server, called `api/msgboard.py`.
 ## Test it out
 
   - `python api/msgboard.py init_db`
-  - `python msgviewer.py`
+  - `python viewer/msgviewer.py`
   - Open http://localhost:8080/ in web browser with
     `Allow-Control-Origin: *`, which you need for
     testing server sent events locally. There's a
     [Chrome Plugin for `Allow-Control-Origin: *`](https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?hl=en)
-    
-## Plans
-
-Here are the features I'm hoping to implement:
-
-  * Nice jinja views for restful calls
-  * Support for checking links, replacing/adding
-    embed for music, video, or picture (javascript)
--- api/tests/msgboard_test.py
+++ api/tests/msgboard_test.py
@@ -1,59 +1,62 @@
+"""Test the msgboard API.
+
+"""
+
 import os
-from .. import msgboard
 import unittest
 import tempfile
 import json
 
+from .. import msgboard
+
+
 class MsgboardTestCase(unittest.TestCase):
 
     def setUp(self):
-
         self.db_fd, msgboard.app.config['DATABASE'] = tempfile.mkstemp()
         msgboard.app.config['TESTING'] = True
         self.app = msgboard.app.test_client()
         msgboard.init_db()
 
     def tearDown(self):
-        
         os.close(self.db_fd)
         os.unlink(msgboard.app.config['DATABASE'])
 
     def test_empty_db(self):
+        """If the database is empty, a GET request
+        to /posts endpoint should return a blank list.
 
-        post_range = {
-                "offset": 0,
-                "limit": 10
-                }
+        """
 
-        data = json.dumps(post_range)
-        response = self.app.get('/posts', data=data,
+        post_range = {"offset": 0, "limit": 10}
+        response = self.app.get('/posts', data=json.dumps(post_range),
                                 content_type="application/json")
-
         assert [] == json.loads(response.data)
 
     def test_create_user(self):
+        """Create a user by POST'ing the correct
+        JSON data to the /user endpoint.
 
-        user_creds = {
-                "username": "testuser",
-                "password": "testpass"
-                }
+        """
 
-        data = json.dumps(user_creds)
-        response = self.app.post('/user', data=data,
+        # Create and send the user data
+        user_data = {"username": "testuser",
+                      "password": "testpass",
+                      "bio": "doopy dops bloop blops",}
+        response = self.app.post('/user', data=json.dumps(user_data),
                                  content_type="application/json")
 
+        # parse the JSON response and remove the created
+        # datetime because that's not predictable
         response = json.loads(response.data)
-
         del response["created"]
-
-        user_fixture = {
-                "username": 'testuser',
-                "bio": None,
-                "id": 1
-                }
-
+        # We expect the response will be this.
+        user_fixture = {"username": "testuser",
+                        "bio": "doopy dops bloop blops",
+                        "id": 1,}
+        # test the expected response vs. actual
         assert user_fixture == response
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     unittest.main()
