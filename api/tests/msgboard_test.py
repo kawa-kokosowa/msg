9b5e7098429df2a3e0b20e1b2874887b1b430e06
talm-quest <thomas.c.almquist@gmail.com>
2016-06-16 15:21:37 -0500

    Added more tests

--- api/tests/msgboard_test.py
+++ api/tests/msgboard_test.py
@@ -58,6 +58,33 @@ class MsgboardTestCase(unittest.TestCase):
 
         assert user_fixture == response
 
+    def test_get_user(self):
+
+        self.test_create_user()
+
+        user_fixture = {
+                "username": 'testuser',
+                "id": 1,
+                "bio": None
+                }
+
+        # First test by using the user_id
+        id_response = self.app.get('/user/1')
+        id_response = json.loads(id_response.data)
+
+        del id_response["created"]
+
+        assert user_fixture == id_response
+
+        # Then test by using the username
+        name_response = self.app.get('/user/testuser')
+        name_response = json.loads(name_response.data)
+
+        del name_response["created"]
+
+        assert user_fixture == name_response
+
+
     def test_post(self):
 
         # i added auth for you , look to the window to the right
@@ -91,6 +118,61 @@ class MsgboardTestCase(unittest.TestCase):
         # you're getting NONE here because the user isn't created
         assert post_fixture == response
 
+    def test_get_post(self):
+
+        self.test_post()
+
+        response = self.app.get('/post/1')
+
+        response = json.loads(response.data)
+
+        del response['created']
+        del response['user']['created']
+
+        post_fixture = {
+                "id": 1,
+                "text": 'I am a post.',
+                "user": {
+                    "bio": None,
+                    "id": 1,
+                    "username": 'testuser'
+                    }
+                }
+
+        assert post_fixture == response
+
+    def test_edit_post(self):
+
+        self.test_get_post()
+
+        edited_post_fixture = {
+                "id": 1,
+                "text": 'I am an edited post.',
+                "user": {
+                    "bio": None,
+                    "id": 1,
+                    "username": 'testuser'
+                    }
+                }
+
+        edit_content = {
+                "text": 'I am an edited post.'
+                }
+
+        b64creds = base64.b64encode("%s:%s" % ("testuser", "testpass"))
+        headers = {"Authorization": "Basic " + b64creds}
+
+        data = json.dumps(edit_content)
+
+        response = self.app.put('/post/1', headers=headers, data=data,
+                                content_type='application/json')
+
+        response = json.loads(response.data)
+
+        del response["created"]
+        del response["user"]["created"]
+
+        assert edited_post_fixture == response
 
 if __name__ == '__main__':
 
