9b0941ef85b7a02a679eb8e1f7d1412255966174
Lily Seabreeze <lillian.gardenia.seabreeze@gmail.com>
2016-07-28 12:41:12 -0500

    setup test

--- setup.py
+++ setup.py
@@ -7,8 +7,22 @@ import sys
 from setuptools import setup
 from distutils.version import StrictVersion
 
+from pip.req import parse_requirements
+
 from msg import __version__
 
+
+# Here I am lazy; I really didn't want to maintain the depends
+# in both requirements files AND in this setup, thus we are
+# going to read them all into a list.
+#
+# CREDIT: http://stackoverflow.com/questions/14399534/how-can-i-reference-requirements-txt-for-the-install-requires-kwarg-in-setuptool
+#
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements/develop.txt', session=False)
+# reqs is a list of requiremens
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='msg',
       packages=['msg'],
